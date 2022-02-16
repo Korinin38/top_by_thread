c259afbf72f334bc2c843b9be8798a16bfbcfab3
Korinin38 <artkash@bk.ru>
2022-02-16 17:49:59 +0300

    Code structuration

--- top_by_thread.py
+++ top_by_thread.py
@@ -1,21 +1,17 @@
 from collections import namedtuple, defaultdict
-# input data
-log_filename = "hz_all_nodes.log"
-top_size = 10
-field_list = ["datetime", "address", "type", "source", "thread", "data"]
 
 
-def read_log(filename, fields):
+def read_log(filename, fields_list):
     '''
     Reads log from given file into manageable format
     :param filename: path to log file
-    :param fields: list of fields of log entry
+    :param fields_list: list of fields of log entry
     :return: list of log entries
     '''
-    Entry = namedtuple("LogEntry", " ".join(fields))
+    Entry = namedtuple("LogEntry", " ".join(fields_list))
     with open(filename, "r") as file:
         lines = file.readlines()
-    list_log = [l.strip().split("\t", len(fields) - 1) for l in lines]
+    list_log = [l.strip().split("\t", len(fields_list) - 1) for l in lines]
     return [Entry(*line) for line in list_log]
 
 
@@ -44,10 +40,18 @@ def print_top(threads_top, count):
         print(*line)
 
 
-if __name__ == '__main__':
-    log = read_log(log_filename, field_list)
-    grouped_by_thread = group_by(log, "thread")
+def top_by_thread(file, top_size, field_list, top_field):
+    log = read_log(file, field_list)
+    grouped_by_thread = group_by(log, top_field)
     thread_count = [(item[0], len(item[1])) for item in grouped_by_thread.items()]
     # lambda: sort in descending order
     thread_top = sorted(thread_count, key=lambda x: -x[1])
     print_top(thread_top, top_size)
+
+
+if __name__ == '__main__':
+    log_filename = "hz_all_nodes.log"
+    size = 10
+    fields = ["datetime", "address", "type", "source", "thread", "data"]
+    top_by = "thread"
+    top_by_thread(log_filename, size, fields, top_by)
